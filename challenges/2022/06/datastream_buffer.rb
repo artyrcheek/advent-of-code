6858d6259ea906b4bc9ec69cef4752c6b781c72e
Arthur Cheek <artyrcheek@gmail.com>
2022-12-06 11:42:34 +0000

    refactoring

--- challenges/2022/06/datastream_buffer.rb
+++ challenges/2022/06/datastream_buffer.rb
@@ -4,16 +4,18 @@ class DatastreamBuffer
   end
 
   def start_of_packet_marker_index
-    start_index = (0..@input.length).find { |i| uniq_chars_at?(i, chars: 4)}
-    start_index + 1 if start_index
+    find_first_uniq_chars_index(amount: 4)&.+1
   end
 
   def start_of_message_marker_index
-    start_index = (0..@input.length).find { |i| uniq_chars_at?(i, chars: 14)}
-    start_index + 1 if start_index
+    find_first_uniq_chars_index(amount: 14)&.+1
   end
 
   private
+
+  def find_first_uniq_chars_index(amount:)
+    (0..@input.length).find { |i| uniq_chars_at?(i, chars: amount)}
+  end
 
   def uniq_chars_at?(index, chars:)
     start = index - chars+1
