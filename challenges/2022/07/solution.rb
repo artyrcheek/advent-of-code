c145566cf0c45828943712e2cef576116dbe9c45
Arthur Cheek <artyrcheek@gmail.com>
2022-12-07 19:43:25 +0000

    Completed part 2 (without readme or tests :( )

--- challenges/2022/07/solution.rb
+++ challenges/2022/07/solution.rb
@@ -5,14 +5,29 @@ module Year2022
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
 
     def part_1
-      tr = TerminalReader.new(data)
-      tr.run
+      tr = new_run
       subdirectories = tr.root_directory.all_subdirectories
       subdirectories.select{|sd| sd.size < 100_000}.sum(&:size)
     end
 
     def part_2
-      nil
+      tr = new_run
+      file_system_size = 70_000_000
+      unused_space_needed = 30_000_000
+
+      used_space = tr.root_directory.size
+      free_space = file_system_size - used_space
+
+      space_needed = unused_space_needed - free_space
+
+      subdirectories = tr.root_directory.all_subdirectories
+      subdirectories.select{|sd| sd.size > space_needed}.min_by(&:size).size
+    end
+
+    def new_run
+      tr = TerminalReader.new(data)
+      tr.run
+      tr
     end
 
   end
