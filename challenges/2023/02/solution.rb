ec8580fd14be3c17eddcbe79a8bdf5cd191d3bac
Arthur Cheek <artyrcheek@gmail.com>
2023-12-02 13:37:25 +0000

    Day 2 part 1

---
+++ challenges/2023/02/solution.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+module Year2023
+  class Day02 < Solution
+    # @input is available if you need the raw data input
+    # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
+
+    def part_1
+      games = data.map { |line| Game.parse(line) }
+      valid_game_ids = games.select { |game| game.sets.all?(&:valid?) }.map(&:game_id)
+      pp valid_game_ids
+      valid_game_ids.map(&:to_i).sum
+    end
+
+    def part_2
+      nil
+    end
+
+    private
+
+    class Game
+      attr_reader :game_id, :sets
+
+      GAME_REGEX = /Game (?<game_id>\d+): (?<sets>.*)/
+
+      def self.parse(line)
+        game_id, sets_string = line.match(GAME_REGEX).captures
+        sets = sets_string.split(";").map { |set_string| Set.parse(set_string) }
+        new(game_id, sets)
+      end
+
+      def initialize(game_id, sets)
+        @game_id = game_id
+        @sets = sets
+      end
+    end
+
+    class Set
+      MAX_COLORS = {
+        red: 12,
+        green: 13,
+        blue: 14,
+      }
+
+      def self.parse(line)
+        colors = MAX_COLORS.map do |key, color|
+          [key, color_count(line, key)]
+        end.to_h
+        new(colors)
+      end
+
+      def self.color_count(line, color)
+        line.scan(/(\d+) #{color}/).flatten.first.to_i
+      end
+
+      def initialize(colors)
+        @colors = colors
+      end
+
+      def valid?
+        @colors.all? { |color, count| count <= MAX_COLORS[color] }
+      end
+    end
+  end
+end
---
+++ spec/2023/02/solution_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+RSpec.describe Year2023::Day02 do
+  let(:input) { File.read(File.join(File.dirname(__FILE__), "../../../challenges/2023/02/input.txt")) }
+  let(:example_input) {
+    <<~EOF
+      Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+      Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+      Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+      Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+      Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    EOF
+  }
+
+  describe "part 1" do
+    it "returns correct answer for the example input" do
+      expect(described_class.part_1(example_input)).to eq(8)
+    end
+  end
+
+  describe "part 2" do
+    # it "returns nil for the example input" do
+    #   expect(described_class.part_2(example_input)).to eq(nil)
+    # end
+
+    # it "returns nil for my input" do
+    #   expect(described_class.part_2(input)).to eq(nil)
+    # end
+  end
+end
